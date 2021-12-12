0596d730fdb5c55254233a7428745122bc687ac9
Nicolai Caruana <oliver.zabbar@gmail.com>
2021-12-12 18:41:22 +0100

    Continued scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public float p1Score;
+    public float p2Score;
+
+    public Text p1Text;
+    public Text p2Text;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        p1Score = 0;
+        p2Score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        p1Text.text = "Player 1: " + p1Score.ToString();
+        p2Text.text = "Player 2: " + p2Score.ToString();
+
+        if (p1Score >= 100f || p2Score >= 100f)
+        {
+            p1Score = 0;
+            p2Score = 0;
+            SceneManager.LoadScene("EndScene");
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,40 @@ public class PlayerController : MonoBehaviour
 {
     public GameManager GM;
 
+    public GameObject p1;
+    public GameObject p2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GM = GameObject.Find("Scripts").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            p1.transform.position = new Vector3(p1.transform.position.x, p1.transform.position.y + 1, p1.transform.position.z);
+            GM.p1Score += 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            p1.transform.position = new Vector3(p1.transform.position.x, p1.transform.position.y + 1, p1.transform.position.z);
+            GM.p1Score += 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            p1.transform.position = new Vector3(p1.transform.position.x, p1.transform.position.y + 1, p1.transform.position.z);
+            GM.p1Score += 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            p1.transform.position = new Vector3(p1.transform.position.x, p1.transform.position.y + 1, p1.transform.position.z);
+            GM.p1Score += 1f;
+        }
     }
 }
