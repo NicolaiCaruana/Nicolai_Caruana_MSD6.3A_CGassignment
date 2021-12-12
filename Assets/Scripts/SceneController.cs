b4176a5ba1504be6b41dd8d5e5194e51faaf3cea
Nicolai Caruana <oliver.zabbar@gmail.com>
2021-12-12 18:05:21 +0100

    Added SceneController and part of GameManager
    Stopped to check why Intellisense is not working well

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void MainGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void EndScene()
+    {
+        SceneManager.LoadScene("EndScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
