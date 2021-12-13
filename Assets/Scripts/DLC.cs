52db297687d58d07b256fdf76071f97d5bb9aa97
Nicolai Caruana <oliver.zabbar@gmail.com>
2021-12-13 15:03:31 +0100

    Final Commit
    Unless I remember anything else

--- Assets/Scripts/DLC.cs
+++ Assets/Scripts/DLC.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Firebase.Database;
 using Firebase.Storage;
+using Firebase.Extensions;
 
 public class DLC : MonoBehaviour
 {
@@ -29,6 +30,28 @@ public class DLC : MonoBehaviour
 
     IEnumerator DownloadImage(StorageReference reference)
     {
+        const long maxAllowedSize = 1 * 1024 * 1024;
+        reference.GetBytesAsync(maxAllowedSize).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsFaulted || task.IsCanceled)
+            {
+                Debug.LogException(task.Exception);
+            }
+
+            else
+            {
+                byte[] Contents = task.Result;
+
+                Texture2D texture = new Texture2D(1, 1);
+                texture.LoadImage(Contents);
+
+                Sprite sprite = Sprite.Create(texture, new Rect(0f, 0f, texture.width, texture.height), new Vector2(1f, 1f), 100f);
+
+                GetComponent<SpriteRenderer>().sprite = sprite;
+            }
+        });
+
+        yield return null;
     }
 
     // Update is called once per frame
